c7df2e4b90dc73bff879661b5b83348b2e7a4065
Jeet267 <152002098+Jeet267@users.noreply.github.com>
2024-11-30 00:11:24 +0530

    Update outcome.js

--- src/components/outcome.js
+++ src/components/outcome.js
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 import men from "../men.png";
 import cir from "../circle.png";
@@ -10,23 +8,14 @@ const Outcome = () => {
 
   return (
     <div className="bg-black min-h-screen flex flex-col items-center justify-center p-4 relative">
-      {/* Home Link */}
       <Link
         to="/"
         className="absolute text-white font-bold top-5 left-5 text-lg md:text-xl"
       >
         Home
       </Link>
-
-      {/* Text Section */}
-      <div className="absolute text-white font-semibold text-center top-16 md:top-24 left-1/2 transform -translate-x-1/2 w-4/5 text-sm md:text-lg lg:text-xl">
-        <p>Your estimated IVF Success Rate is</p>
-      </div>
-
-      {/* Circle and Men Image Section (Horizontal Alignment) */}
-      <div className="flex flex-col md:flex-row items-center justify-center gap-8 mt-20 md:mt-28 lg:mt-36">
-        {/* Circle with Percentage */}
-        <div className="relative">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-8 mt-20 md:mt-28 lg:mt-36 relative">
+        <div className="relative ml-6">
           <img
             src={cir}
             alt="Circle Logo"
@@ -37,13 +26,12 @@ const Outcome = () => {
           </p>
         </div>
 
-        {/* Men Image */}
-        <div className="relative w-[200px] h-[200px] md:w-[250px] md:h-[250px] lg:w-[350px] lg:h-[350px] overflow-hidden">
-          <img
-            src={men}
-            alt="Men"
-            className="w-full h-full object-cover"
-          />
+        <div className="absolute text-white font-semibold text-center top-[-30px] md:top-[-30px] left-1/2 md:left-auto md:right-[-15%] transform -translate-x-1/2 w-full text-sm md:text-lg lg:text-xl">
+          <p>Your estimated IVF Success Rate is</p>
+        </div>
+
+        <div className="relative w-[400px] h-[300px] md:w-[500px] md:h-[400px] lg:w-[800px] lg:h-[450px] overflow-hidden">
+          <img src={men} alt="Men" className="w-full h-full object-cover" />
         </div>
       </div>
     </div>
@@ -51,3 +39,4 @@ const Outcome = () => {
 };
 
 export default Outcome;
+
